b11b48b2565427bf0fe80ef3764c358be9c1d6fa
shuhelahm <shuhel037@gmail.com>
2022-03-11 11:37:22 +0600

    footer updated

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,9 +5,10 @@ const Footer = () => {
     return (
         <>
         <section className='w-100 bg-dark'>
-            <div className='footer-container text-center w-50 mx-auto py-4 bg-dark text-white'>
+            <div className='footer-container text-center w-75 mx-auto py-4 bg-dark text-white'>
                 <h5 style={{fontWeight: 'bold'}}>CAM PAVILION</h5>
                 <p style={{fontSize: '12px'}}>We created Cam Pavilion because we saw the need for a better way to buy and sell photography and videography equipment. Passionate about the craft ourselves, we dreamed of a more focused platform designed for industry professionals and hobbyists.</p>
+                <small>Copyright &copy; 2022 || Cam Pavilion</small>
             </div>
         </section>
         </>
