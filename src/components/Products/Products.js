93525fc616378fe3a3e6d82d62892b72c15fbc82
shuhelahm <shuhel037@gmail.com>
2022-05-03 06:33:58 +0600

    updated

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -32,9 +32,9 @@ const Products = () => {
 
          :  
         <div>
-            <div className='container'>
+            <div className='container mx-auto'>
                 <h2 className='mt-5 mb-3' style={{fontWeight: "bold",width:"80%"}}>OUR <span className='yellow-text'>PRODUCTS</span></h2>
-                <div className='products-container row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4 justify-content-between'>
+                <div className='products-container row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4'>
                     {
                         products.map(product=> <Product key={product._id} product={product}></Product>)
                     }
